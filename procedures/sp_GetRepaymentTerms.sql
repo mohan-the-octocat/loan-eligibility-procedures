70aeafc27f264f15ce4da4a4b79e0f09c3c2a7b6
Mohan Sridharan <83639549+mohan-the-octocat@users.noreply.github.com>
2025-07-03 16:27:28 +0530

    Migrate sp_GetRepaymentTerms to Cloud SQL

--- procedures/sp_GetRepaymentTerms.sql
+++ procedures/sp_GetRepaymentTerms.sql
@@ -1,12 +1,12 @@
-CREATE PROCEDURE sp_GetRepaymentTerms(
-    IN loan_amount DECIMAL(10,2)
+CREATE OR REPLACE PROCEDURE sp_GetRepaymentTerms(
+    loan_amount NUMERIC
 )
 BEGIN
     SELECT 
         term_months,
         interest_rate,
-        loan_amount * (interest_rate/12 * POWER(1 + interest_rate/12, term_months)) / 
-        (POWER(1 + interest_rate/12, term_months) - 1) as monthly_payment
+        loan_amount * (interest_rate/12 * POW(1 + interest_rate/12, term_months)) / 
+        (POW(1 + interest_rate/12, term_months) - 1) as monthly_payment
     FROM repayment_terms
     WHERE min_amount <= loan_amount AND max_amount >= loan_amount;
 END;
